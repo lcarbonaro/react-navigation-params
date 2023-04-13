c6ce655f20e78f5854856cfa9f8b5b8a78bc6181
Les Carbonaro <lcarbonaro@gmail.com>
2023-04-12 20:18:33 -0400

    initial commit

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 
 function Details() {
-    //const navigate = useNavigate();  // not allowed !?!
-    /*
+  //const navigate = useNavigate();  // not allowed !?!
+  /*
     return (
         <div className="myCardDetails">
             <p>hello world from id: {params.id}</p>
@@ -14,19 +14,49 @@ function Details() {
     );
     */
 
-    const params = useParams();
-    
-    return (
-        <div className="myCardDetails">
-            <p>hello world from id: {params.id}</p>
+  const params = useParams();
 
-            <Link to="/">
-                <button >Go Back</button>
-            </Link>
-            
-        </div>
-    );
+  const hdgData = ["Detail", "Value"];
+  const rowData = [
+    {
+      name: "City",
+      value: "Mississauga",
+    },
+    {
+      name: "Age",
+      value: 23,
+    },
+  ];
 
+  return (
+    <div className="myCardDetails">
+      <p>hello world from id: {params.id}</p>
+
+      <table>
+        <thead>
+          <tr>
+            {hdgData.map((h, i) => {
+              return <th key={i}>{h}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rowData.map((r, i) => {
+            return (
+              <tr key={i}>
+                <td>{r.name}</td>
+                <td>{r.value}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <Link to="/">
+        <button>Go Back</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Details;
